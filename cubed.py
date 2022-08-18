b32034ae75b2684a1b0a1490d44bfa8b220af7be
George Grunfelder <68717396+GitGrunfelder@users.noreply.github.com>
2022-08-17 20:57:44 -0400

    ch 8

---
+++ cubed.py
@@ -0,0 +1,4 @@
+#to be used in self taught 8
+#define a function that cubes
+def cubed(x):
+    return x * x * x
--- self_taught_8.py
+++ self_taught_8.py
@@ -1,1 +1,15 @@
-print("HI")
+import statistics
+#import my cubed module
+import cubed
+
+#use a method from statistics
+x = [1,5,10]
+y = [2,99,11]
+z = statistics.correlation(x,y)
+print(z)
+
+#create a module called cubed that takes a # and returns it cubed, import and use.
+#from cubed, use method cubed on 3, assign to answer. Print
+answer = cubed.cubed(3)
+print(answer)
+
